400562bb1f2868b2509136470e0b66284c8eb146
Lau-rent03 <Laurentpimentelacc@gmail.com>
2025-12-04 17:24:10 -0300

    Desenvolvido em sala, classes;

---
+++ Competidor.js
@@ -0,0 +1,29 @@
+import { Atleta } from "./Atleta.js";
+import { Competicao } from "./Competicao.js";
+export class Competidor {
+
+    //valor tem que estar  em competicao ou competidor?
+
+    
+    static #numeroPeito;
+    #data_inscricao;
+    #atleta;
+
+    constructor(data_inscricao, atleta) {
+
+        this.#data_inscricao = data_inscricao;
+
+        Competidor.#numeroPeito = Number(("" + Competicao.quantidadeInscritos + Atleta.data_nasc.getFullYear()).padEnd(6,'0')); //concatena a quantidade de inscritos com o ano de nascimento do atleta
+
+    }
+
+
+    get data_inscricao() {
+        return this.#data_inscricao;
+    }
+
+    get numeroPeito() {
+        return Competidor.#numeroPeito;
+    }
+
+}

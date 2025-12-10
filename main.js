5c0112e90a43fb6948af5ec04e045258fb6a287b
Lau-rent03 <Laurentpimentelacc@gmail.com>
2025-12-10 00:42:00 -0300

    criação dos html e o main js

---
+++ main.js
@@ -0,0 +1,19 @@
+import * as atletaController from './js/controllers/AtletaController.js';
+import * as competicaoController from './js/controllers/CompeticaoController.js';
+import * as inscricoesController from './js/controllers/InscricoesController.js';
+import * as relatoriosController from './js/controllers/RelatoriosController.js';
+import * as dashboardController from './js/controllers/DashboardController.js';
+
+const currentPage = window.location.pathname;
+
+if (currentPage.includes('atletas.html')) {
+    atletaController.inicializar();
+} else if (currentPage.includes('competicoes.html')) {
+    competicaoController.inicializar();
+} else if (currentPage.includes('inscricoes.html')) {
+    inscricoesController.inicializar();
+} else if (currentPage.includes('relatorios.html')) {
+    relatoriosController.inicializar();
+} else if (currentPage.includes('index.html') || currentPage.endsWith('/')) {
+    dashboardController.inicializar();
+}
